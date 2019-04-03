7ff38a2dd0699584965143b7a9f460e8b680985f
QuyenNguyen <cuonbinhthuan@gmail.com>
2019-04-04 01:49:37 +0700

    1:49 4/3/2019

--- 31161021458_NguyenTuQuyen/Controllers/ProductsController.cs
+++ 31161021458_NguyenTuQuyen/Controllers/ProductsController.cs
@@ -5,6 +5,7 @@ using System.Data.Entity;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 using _31161021458_NguyenTuQuyen.Models;
@@ -158,6 +159,146 @@ namespace _31161021458_NguyenTuQuyen.Controllers
                 db.Dispose();
             }
             base.Dispose(disposing);
+        }
+        //Add to cart
+        public ActionResult AddToCart(int id)
+        {
+            //Kiem tra Id movie ton tai hay khong
+            var product = db.Products.Where(x => x.ID == id).FirstOrDefault();
+            if (product == null)
+            {
+                return RedirectToAction("Index");
+            }
+            var hoaDon = this.Session["HoaDon"] as HoaDon;
+            if (hoaDon == null)
+            {
+                hoaDon = new HoaDon();
+                hoaDon.NgayLap = DateTime.Now;
+                hoaDon.ChiTietHoaDons = new List<ChiTietHoaDon>();
+                this.Session["HoaDon"] = hoaDon;
+                db.HoaDons.Add(hoaDon);
+            }
+            //Kiem tra don hang da co truoc do
+            var chiTietHoaDon = hoaDon.ChiTietHoaDons.Where(x => x.ProductObj.ID == id).FirstOrDefault();
+            if (chiTietHoaDon == null)
+            {
+                chiTietHoaDon = new ChiTietHoaDon();
+                chiTietHoaDon.MaProduct = id;
+                chiTietHoaDon.ProductObj = product;
+                chiTietHoaDon.HoaDonObj = hoaDon;
+                chiTietHoaDon.SoLuong = 1;
+                hoaDon.ChiTietHoaDons.Add(chiTietHoaDon);
+            }
+            else
+            {
+                chiTietHoaDon.SoLuong++;
+            }
+            hoaDon.TongTien =
+                (double)hoaDon.ChiTietHoaDons
+                .Sum(x => x.SoLuong * x.ProductObj.Price);
+            db.SaveChanges();
+            return View(hoaDon);
+        }
+        //tang giam so luong
+        public ActionResult TangSoLuong(int id)
+        {
+            //Kiem tra Id product ton tai hay khong
+
+            var hoaDon = this.Session["HoaDon"] as HoaDon;
+
+            //Kiem tra don hang da co truoc do
+            var chiTietHoaDon = hoaDon.ChiTietHoaDons.Where(x => x.ProductObj.ID == id).FirstOrDefault();
+
+            chiTietHoaDon.SoLuong++;
+
+            hoaDon.TongTien =
+                (double)hoaDon.ChiTietHoaDons
+                .Sum(x => x.SoLuong * x.ProductObj.Price);
+            db.SaveChanges();
+            return View("AddToCart", hoaDon);
+        }
+        public ActionResult GiamSoLuong(int id)
+        {
+            //Kiem tra Id product ton tai hay khong
+
+            var hoaDon = this.Session["HoaDon"] as HoaDon;
+
+            //Kiem tra don hang da co truoc do
+            var chiTietHoaDon = hoaDon.ChiTietHoaDons.Where(x => x.ProductObj.ID == id).FirstOrDefault();
+
+            chiTietHoaDon.SoLuong--;
+
+            hoaDon.TongTien =
+                (double)hoaDon.ChiTietHoaDons
+                .Sum(x => x.SoLuong * x.ProductObj.Price);
+            db.SaveChanges();
+            return View("AddToCart", hoaDon);
+        }
+
+        public ActionResult RemoveFromCart(int maMovies)
+        {
+            var hoaDon = this.Session["HoaDon"] as HoaDon;
+            var chiTietHoaDon = hoaDon.ChiTietHoaDons.Where(x
+            => x.ProductObj.ID == maMovies).FirstOrDefault();
+            hoaDon.ChiTietHoaDons.Remove(chiTietHoaDon);
+            return View("AddToCart", hoaDon);
+        }
+
+        public PartialViewResult Summary()
+        {
+            var hoaDon = this.Session["HoaDon"] as HoaDon;
+            if (hoaDon == null)
+            {
+                return null;
+            }
+            return PartialView(hoaDon);
+        }
+
+        [Authorize]
+        public ActionResult Checkout()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Checkout(ShippingDetail detail)
+        {
+            var hoaDon = this.Session["HoaDon"] as HoaDon;
+            if (hoaDon.ChiTietHoaDons.Count() == 0)
+            {
+                ModelState.AddModelError("", "Sorry, your cart is empty!");
+            }
+            if (ModelState.IsValid)
+            {
+                StringBuilder body = new StringBuilder()
+                .AppendLine("A new order has been submitted")
+                .AppendLine("---")
+                .AppendLine("Items:");
+                foreach (var hoaDonChiTiet in hoaDon.ChiTietHoaDons)
+                {
+                    var subtotal = hoaDonChiTiet.ProductObj.Price * hoaDonChiTiet.SoLuong;
+                    body.AppendFormat("{0} x {1} (subtotal: {2:c}", hoaDonChiTiet.SoLuong,
+                    hoaDonChiTiet.ProductObj.NameProduct,
+                    subtotal);
+                }
+                body.AppendFormat("Total order value: {0:c}", hoaDon.TongTien)
+                .AppendLine("---")
+                .AppendLine("Ship to:")
+                .AppendLine(detail.Name)
+                .AppendLine(detail.Address)
+                .AppendLine(detail.Mobile.ToString());
+                //EmailServiceNew.SendEmail(new IdentityMessage()
+                //{
+                //    Destination = detail.Email,
+                //    Subject = "New order submitted!",
+                //    Body = body.ToString()
+                //});
+                this.Session["HoaDon"] = null;
+                return View("CheckoutCompleted");
+            }
+            else
+            {
+                return View(new ShippingDetail());
+            }
         }
     }
 }
